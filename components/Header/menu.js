e9b6f70bf65be227a8c9ce375dcc2177b11df59e
Krish Sapru <67235656+ksapru@users.noreply.github.com>
2021-01-26 18:23:30 -0500

    Add files via upload

---
+++ components/Header/menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import DropDownItem from 'react-native-drop-down-item';
+
+state = {
+    contents: [
+      {
+        title: 'Title 1',
+        body: 'Hi. I love this component. What do you think?',
+      },
+      {
+        title: 'See this one too',
+        body: 'Yes. You can have more items.',
+      },
+      {
+        title: 'Thrid thing',
+        body: 'What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text?',
+      },
+    ],
+  };
